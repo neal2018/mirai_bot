b2b85d623ae29354c0fa041bde33b43bd78a8294
neal2018 <neal.lion@icloud.com>
2020-12-18 01:44:48 +0800

    add some feature

--- src/main/kotlin/com/example/MyLoader.kt
+++ src/main/kotlin/com/example/MyLoader.kt
@@ -1,5 +1,6 @@
 package com.example
 
+import com.beust.klaxon.JsonArray
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.alsoLogin
 import net.mamoe.mirai.join
@@ -10,44 +11,63 @@ import net.mamoe.mirai.message.data.content
 import com.beust.klaxon.JsonObject
 import com.beust.klaxon.Parser
 import java.io.File
+import java.net.URL
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 suspend fun main() {
     val dataPath = System.getProperty("user.dir") + File.separator + "src/main/kotlin/com/example/cardData"
-
-    val cardInfo = loadJson(dataPath, "cardInfo")
-    val eggInfo = loadJson(dataPath, "eggInfo")
-    val nickNameInfo = loadJson(dataPath, "nickNameInfo")
-
+//    BotConfiguration.deviceInfo
     val qqId = 2221744851L//Bot的QQ号，需为Long类型，在结尾处添加大写L
-    val password = "********"//Bot的密码
-    val miraiBot = Bot(qqId, password).alsoLogin()//新建Bot并登录
+    val password = "*****"//Bot的密码
+    val miraiBot = Bot(qqId, password){
+        fileBasedDeviceInfo("device.json") // 使用 "device.json" 保存设备信息
+    }.alsoLogin()//新建Bot并登录
     miraiBot.subscribeMessages {
         (startsWith("/searchcard") or startsWith("/sc")) {
-            reply(getSearchCardMessage(message.content, cardInfo, eggInfo, nickNameInfo))
+            reply(getSearchCardMessage(message.content, dataPath))
         }
 
         (startsWith("/info")) {
-            reply(getInfoMessage(message.content))
+            reply(getInfoMessage(message.content, dataPath))
         }
 
         (startsWith("/help") or startsWith("/h")) {
-            reply(getHelpMessage())
+            reply(getHelpMessage(dataPath))
         }
 
         (startsWith("/welcome") or startsWith("/w")) {
-            reply(getWelcomeMessage())
+            reply(getWelcomeMessage(dataPath))
         }
 
         (startsWith("/baidu") or startsWith("/bd")) {
             reply(getSearchBaiduMessage(message.content))
         }
+
+        (startsWith("/online") or startsWith("/ol")) {
+            reply(getOnlineMessage())
+        }
+
+        (startsWith("/api") or startsWith("/Api") or startsWith("/API")) {
+            reply(getAPIMessage())
+        }
+
+        (startsWith("/update")) {
+            updateJson(dataPath)
+            reply("更新中...")
+        }
     }
 
     miraiBot.subscribeAlways<MemberJoinEvent> {
-        it.group.sendMessage(getWelcomeMessage())
+        it.group.sendMessage(getWelcomeMessage(dataPath))
     }
 
     miraiBot.join() // 等待 Bot 离线, 避免主线程退出
+}
+
+fun updateJson(dataPath: String) {
+//    Runtime.getRuntime().exec(dataPath + File.separator + "update.sh")
 }
 
 
@@ -61,9 +81,7 @@ fun loadJson(dataPath: String, name: String): JsonObject {
 
 fun getSearchCardMessage(
     messageContent: String,
-    cardInfo: JsonObject,
-    eggInfo: JsonObject,
-    nickNameInfo: JsonObject
+    dataPath: String
 ): String {
     val searchContent = when {
         messageContent.startsWith("/searchcard") -> {
@@ -76,6 +94,10 @@ fun getSearchCardMessage(
             return "[INFO] 错误格式的命令。"
         }
     }
+
+    val cardInfo = loadJson(dataPath, "cardInfo")
+    val eggInfo = loadJson(dataPath, "eggInfo")
+    val nickNameInfo = loadJson(dataPath, "nickNameInfo")
 
     if (searchContent in cardInfo) {
         return getCardInfo(cardInfo[searchContent] as JsonObject)
@@ -155,7 +177,7 @@ fun getSearchBaiduMessage(messageContent: String): String {
     return "[INFO] 点击 http://www.baidu.com/s?wd=$encodedContent 查看搜索结果！"
 }
 
-fun getInfoMessage(messageContent: String): String {
+fun getInfoMessage(messageContent: String, dataPath: String): String {
     val searchContent = when {
         messageContent.startsWith("/info") -> {
             messageContent.substringAfter("/info").trim()
@@ -165,58 +187,50 @@ fun getInfoMessage(messageContent: String): String {
         }
     }
 
-    return if (searchContent.contains("文档")) {
-        """
-        [INFO] DIY服务器更新内容在：https://shimo.im/docs/TQdjjwpPwd9hJhKc
-        DIY的修改意见在：https://shimo.im/docs/hRIn0C91IFUYZZ6n
-        讨论区在：https://github.com/LegacyGwent/LegacyGwent/issues
-        欢迎大家踊跃参与！
-    """.trimIndent()
-    } else if (searchContent.contains("在线") || searchContent.contains("online")) {
-        """
-        [INFO] DIY服的在线人数在：http://cynthia.ovyno.com:5005/
-        原服的在线人数在：http://cynthia.ovyno.com:5000/
-        欢迎大家一起打牌！
-    """.trimIndent()
-    } else if (searchContent.contains("下载")) {
-        """
-        [INFO] 原版服务器下载地址：群文件/客户端
-        DIY服下载地址：http://cynthia.ovyno.com:5005/download
-        zip结尾的是电脑版，apk结尾的是安卓版，dmg结尾的是mac版
-        小助手欢迎大家下载！
-    """.trimIndent()
-    } else if (searchContent.contains("讨论")) {
-        "[INFO] https://github.com/LegacyGwent/LegacyGwent/issues"
-    } else if (searchContent.contains("贴吧")) {
-        "[INFO] https://tieba.baidu.com/f?ie=utf-8&kw=%E6%80%80%E6%97%A7%E6%98%86%E7%89%B9%E7%89%8C"
-    } else if (searchContent.contains("匹配码")) {
-        """
-        [INFO] 匹配界面右下角可以输入匹配码，匹配码一样的玩家可以相互好友对战！
-        匹配码输入ai或者ai1可以匹配ai，但会优先匹配在线玩家。使用ai#f或者ai1#f可以强制匹配ai。
-    """.trimIndent()
-    } else if (searchContent.contains("repo")) {
-        "[INFO] 小助手开源在：https://github.com/neal2018/mirai_bot"
-    } else {
-        "[INFO] /info后面可以输入【文档】、【在线】、【匹配码】、【讨论】、【贴吧】或者【下载】查看相关内容！"
+    val info = loadJson(dataPath, "info.json")
+    if (searchContent == "在线" || searchContent == "online") {
+        return getOnlineMessage()
     }
+    if (searchContent in info) {
+        return info[searchContent] as String
+    }
+
+    return "[INFO] /info后面可以输入【文档】、【在线】、【匹配码】、【讨论】、【贴吧】或者【下载】查看相关内容！"
 }
 
-fun getHelpMessage(): String {
-    return """[HELP] 发送以下命令可以触发相应效果:
-/help 或 /h: 显示本信息
-/info: 显示相关的信息
-/welcome: 发送入群欢迎信息
-/searchcard 或 /sc + 关键词: 搜索卡牌效果
-/baidu 或 /bd + 关键词: 使用百度搜索
-小助手期待大家多多打牌！"""
+fun getOnlineMessage(): String {
+    val extra = try {
+        val text = URL("http://cynthia.ovyno.com:5005/api/gwentdata/onlineinfo").readText()
+        val parser = Parser.default()
+        val json = parser.parse(StringBuilder(text)) as JsonObject
+        val userList = json["user"] as JsonArray<*>
+        when {
+            (userList.size == 0) -> "现在DIY服没有人在线...QAQ\n"
+            else -> "现在DIY服有${userList.size}人在线！\n"
+        }
+    } catch (ex: Exception) {
+        ""
+    }
+
+    return """[INFO] $extra DIY服的在线人数在：http://cynthia.ovyno.com:5005/
+原服的在线人数在：http://cynthia.ovyno.com:5000/
+欢迎大家一起打牌！"""
 }
 
-fun getWelcomeMessage(): String {
-    return """[WELCOME] 新人好！群里主要讨论老昆特相关事宜！
-原版服务器下载地址: 群文件/客户端
-DIY服下载地址: http://cynthia.ovyno.com:5005/download
-zip结尾的是电脑版，apk结尾的是安卓版，dmg结尾的是mac版
-DIY的修改意见在: https://shimo.im/docs/hRIn0C91IFUYZZ6n
-讨论区在: https://github.com/LegacyGwent/LegacyGwent/issues
-期待你的参与！"""
+fun getAPIMessage(): String {
+    val dateFormatter: DateFormat = SimpleDateFormat("yyyy-MM-dd")
+    val today = dateFormatter.format(Date())
+    val list = listOf<String>("queryranking", "queryenvironment", "querymatches", "querycard")
+    val prefix = "http://cynthia.ovyno.com:5005/api/gwentdata/"
+    return list.fold("") { acc, string -> "$acc\n$prefix$string/$today" }
+}
+
+fun getHelpMessage(dataPath: String): String {
+    val info = loadJson(dataPath, "info.json")
+    return info["help"] as String
+}
+
+fun getWelcomeMessage(dataPath: String): String {
+    val info = loadJson(dataPath, "info.json")
+    return info["welcome"] as String
 }
